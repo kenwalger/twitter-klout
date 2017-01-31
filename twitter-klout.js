d262a02e890061e5dd902b9c58c578ee0ca4f63d
Andrew Chalkley <andrew@chalkley.org>
2017-01-30 21:23:31 -0800

    Added callback

--- twitter-klout.js
+++ twitter-klout.js
@@ -16,70 +16,71 @@ function get(twitterUser) {
 	// Connect to the Klout API with the Twitter User name
 	// http://api.klout.com/v2/identity.json/twitter?screenName={twitterUser}&key={KLOUT-API-KEY}
 	var request = http.get("http://api.klout.com/v2/identity.json/twitter?screenName="
-		+ twitterUser + "&key=" + process.env.KLOUT-API, function(response){
-		var body = "";
-		// Read the data
-		response.on('data', function(chunk) {
-			body += chunk;
-		});
-		response.on('end', function() {
-			if(response.statusCode === 200) {
-				try {
-					// Parse the data
-					var userInfo = JSON.parse(body);
-					// Get Klout Score and print the message
-					klout = getKlout(userInfo.id);
-					printMessage(
-						twitterUser,
-						klout.score,
-						klout.scoreDelta.dayChange,
-						klout.scoreDelta.weekChange,
-						klout.scoreDelta.monthChange
-					);
-				} catch(error) {
-					// Parse error
-					printError(error);
+		+ twitterUser + "&key=" + process.env.KLOUT_API, function (response) {
+			var body = "";
+			// Read the data
+			response.on('data', function (chunk) {
+				body += chunk;
+			});
+			response.on('end', function () {
+				if (response.statusCode === 200) {
+					try {
+						// Parse the data
+						var userInfo = JSON.parse(body);
+						// Get Klout Score and print the message
+						getKlout(userInfo.id, function (klout) {
+							printMessage(
+								twitterUser,
+								klout.score,
+								klout.scoreDelta.dayChange,
+								klout.scoreDelta.weekChange,
+								klout.scoreDelta.monthChange
+							);
+						});
+					} catch (error) {
+						// Parse error
+						printError(error);
+					}
+				} else {
+					// Status Code Error
+					printError({
+						message: "There was an error getting the information for " + twitterUser + ". ("
+						+ http.STATUS_CODES[response.statusCode] + ") "
+					});
 				}
-			} else {
-				// Status Code Error
-				printError({
-					message: "There was an error getting the information for " + twitterUser + ". ("
-					+ http.STATUS_CODES [response.statusCode] + ") "
-				});
-			}
+			});
 		});
-	});
 	// Connection Error
 	request.on("error", printError);
 }
 
-function getKlout(id) {
+function getKlout(id, callback) {
 	// Connect to Klout API with id
 	// http://api.klout.com/v2/user.json/{id}/score?key={KLOUT-API-KEY}
 	var request = http.get("http://api.klout.com/v2/user.json/"
-		+ id +"/score?key=" + process.env.KLOUT-API, function (response) {
-		var body = "";
-		// Read the data
-		response.on('data', function(chunk) {
-			body += chunk;
-		});
-		response.on('end', function() {
-			if(response.statusCode === 200) {
-				try {
-					return JSON.parse(body);
-				} catch (error) {
-					// Parse Error
-					printError(error);
+		+ id + "/score?key=" + process.env.KLOUT_API, function (response) {
+			var body = "";
+			// Read the data
+			response.on('data', function (chunk) {
+				body += chunk;
+			});
+			response.on('end', function () {
+				if (response.statusCode === 200) {
+					try {
+						callback(JSON.parse(body));
+					} catch (error) {
+						// Parse Error
+						printError(error);
+					}
+				} else {
+					// Status Code Error
+					printError({
+						message: "There was an error getting the Klout information of " + id + ". ("
+						+ http.STATUS_CODES[response.statusCode] + ") "
+					});
 				}
-			} else {
-				// Status Code Error
-				printError({
-					message: "There was an error getting the Klout information of " + id + ". ("
-					+ http.STATUS_CODES [response.statusCode] + ") "
-				});
-			}
+			});
 		});
-	});
 	// Connection Error
 	request.on("error", printError);
 }
